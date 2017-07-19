ad7cffc1a7539f7774bd4e73da810a068b65fc60
Djob <jordan.nguyenno@gmail.com>
2017-07-19 19:17:33 +0000

    creation espace profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   protected
  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar, :phone_number, :description])
  end
   
 end
