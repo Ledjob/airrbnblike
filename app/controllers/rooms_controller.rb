f56d6c1987b56a4519e650b5e7efb2d69c75c831
Djob <jordan.nguyenno@gmail.com>
2017-07-22 13:50:10 +0000

    modification controller photos

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
     
 before_action :set_room, only: [:show, :edit, :update]
 before_action :authenticate_user!, except: [:show]
+before_action :require_same_user, only: [:edit, :update]
     
     def index
          @rooms = current_user.rooms 
@@ -14,27 +15,41 @@ before_action :authenticate_user!, except: [:show]
     def create
         @room = current_user.rooms.build(room_params)
         if @room.save
-            redirect_to @room, notice:"Your casa has been successfully added" #redirect to ad page and notify user 
+            if params[:images]
+                params[:images].each do |i|
+                    @room.photos.create(image: i)
+                end
+            end
+            @photos = @room.photos
+            redirect_to edit_room_path(@room), notice:"Your casa has been successfully added" #redirect to ad page and notify user 
         else
             render :new 
         end
     end
     
     def show
+        @photos = @room.photos 
     end
  
     def edit
-
+        @photos = @room.photos
     end
  
     def update
-       if @room.update(room_params)
-            redirect to @room, notice:"Change has been saved..."
+        if @room.update(room_params)
+            if params[:images]
+                params[:images].each do |i|
+                    @room.photos.create(image: i)
+                end
+            end
+            @photos = @room.photos 
+            redirect_to edit_room_path(@room), notice:"Change has been saved..."
+        else
            render :edit
-       end
+        end
     end
     
-    private
+private
       def set_room
          @room = Room.find(params[:id])
       end
@@ -44,6 +59,18 @@ before_action :authenticate_user!, except: [:show]
         params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room,
         :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet, :is_shampoo, :is_breakfast, :is_heating, :is_air,:is_smoker, :is_pet_allowed, :is_kitchen, :price, :active)
         end
+        
+        def require_same_user
+ 
+          if current_user.id != @room.user_id
+ 
+               flash[:danger] = "You don't have the right to modify this page"
+ 
+               redirect_to root_path
+ 
+          end
+ 
+ end
  
  
 end
