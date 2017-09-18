42dc1302bb56da8c19e6ca44ffedc710163f2c9c
Djob <jordan.nguyenno@gmail.com>
2017-09-18 21:35:59 +0000

    Add model review

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,7 @@ class Room < ActiveRecord::Base
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
   
   validates :home_type, presence: true  #la variable type d’habitation sera nécessaire pour la création d’une annonce  
                validates :room_type, presence: true
@@ -16,4 +17,8 @@ class Room < ActiveRecord::Base
                
                geocoded_by :address
                after_validation :geocode, if: :address_changed?
+               
+               def average_rating
+                reviews.count == 0 ? 0 : reviews.average(:star).round(2)
+               end
 end
