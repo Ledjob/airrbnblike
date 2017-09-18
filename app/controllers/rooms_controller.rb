6284e6a65695b2547801dbfd4ace9e1db736e050
Djob <jordan.nguyenno@gmail.com>
2017-09-18 21:47:30 +0000

    Add controller review

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -29,6 +29,12 @@ before_action :require_same_user, only: [:edit, :update]
     
     def show
         @photos = @room.photos 
+        
+        @reviews = @room.reviews
+        if  current_user
+          @booked = Reservation.where("room_id = ? AND user_id = ?", @room.id, current_user.id).present? 
+          @hasReview = @reviews.find_by(user_id: current_user.id)
+        end
     end
     
     def destroy
