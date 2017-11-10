13b80d14349c4ca30f8cbfcbb35bc5fb7e8b2554
Djob <jordan.nguyenno@gmail.com>
2017-11-10 23:03:21 +0000

    mail sendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => 'airrbnblike-djob.c9users.io:8080'}
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
